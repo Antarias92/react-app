2fb8e1b5d8ae26a2b07db47377083366dfe68af4
Anthony Arias <aarias92@gmail.com>
2025-03-27 14:30:52 -0500

    installed and imported bootstrap and react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
+      <h1>Hello World</h1>
     </div>
   );
 }
